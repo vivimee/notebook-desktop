7c7174193feaba68c4930eb2f009708eebe6cc6d
彭梦梦 <pengmengmeng@meituan.com>
2020-03-28 18:18:57 +0800

    update

--- app/reducers/notebook.ts
+++ app/reducers/notebook.ts
@@ -50,30 +50,28 @@ export default function notebook(state = initialState, action: Action<string>) {
       };
     case UPDATE_ARTICLE_CONTENT:
       const { article, content, book } = action.payload;
-      const books = state.books.map(bok => {
-        if (bok.id === book.id) {
-          return {
-            ...bok,
-            articles: bok.articles.map(art => {
-              if (art.id === article.id) {
-                return {
-                  ...art,
-                  content
-                };
-              }
-              return art;
-            })
-          };
-        }
+      const newArticle = {
+        ...article,
+        content
+      };
+      const newBook = {
+        ...book,
+        articles: book.articles.map((art) => {
+          if (art.id === article.id) {
+            return newArticle;
+          }
+          return art;
+        })
+      };
+      const newBooks = state.books.map((bok) => {
+        if (bok.id === book.id) return newBook;
         return bok;
       });
       return {
         ...state,
-        books,
-        activeArticle: {
-          ...article,
-          content
-        }
+        books: newBooks,
+        activeBook: newBook,
+        activeArticle: newArticle
       };
     default:
       return state;
